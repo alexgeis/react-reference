cd0e30f4f06e7b2cde9e50cae56dda04feadf2ad
Alex Geis <siegxela@gmail.com>
2023-03-27 12:32:53 -0600

    /pages/_HOME - update HomePage.tsx with boilerplate jsx

--- react-ref/src/pages/_HOME/HomePage.tsx
+++ react-ref/src/pages/_HOME/HomePage.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactLogo from "@/assets/react.svg";
+import "./HomePage.css";
 
 const styles: any = {
+	App: {},
 	container: {
 		height: "100%",
 		width: "100%",
@@ -16,32 +18,52 @@ export default function HomePage() {
                 Main content
                 Footer
             */}
+			<div style={styles.App}></div>
 			<header style={styles.container}>
 				{/* hamburger */}
-				<img
-					src={ReactLogo}
-					alt="React Logo"
-				/>
-				{/* <div>SEARCH</div> */}
-				<nav>
-					<ul>
-						<li>
-							<a href="link1.html">link 1</a>
-						</li>
-						<li>
-							<a href="link2.html">link 2</a>
-						</li>
-						<li>
-							<a href="link3.html">link 3</a>
-						</li>
-						<li>
-							<a href="link4.html">link 4</a>
-						</li>
-					</ul>
+				<div className="logoSearchWrapper">
+					<img
+						src={ReactLogo}
+						alt="React Logo"
+					/>
+					<div>SEARCH</div>
+				</div>
+				<div className="navWrapper">
+					<nav>
+						<ul>
+							<li>
+								<a href="link1.html">link 1</a>
+							</li>
+							<li>
+								<a href="link2.html">link 2</a>
+							</li>
+							<li>
+								<a href="link3.html">link 3</a>
+							</li>
+							<li>
+								<a href="link4.html">link 4</a>
+							</li>
+						</ul>
+					</nav>
+
 					{/* light / dark toggle */}
-					{/* <img src="" alt="GitHub Repository Link" /> */}
-				</nav>
+					<img
+						src=""
+						alt="Theme Toggle"
+					/>
+					{/* github */}
+					<img
+						src=""
+						alt="GitHub Repository Link"
+					/>
+				</div>
 			</header>
+
+			<main>
+				<section className="hero"></section>
+			</main>
+
+			<footer className="footer">Footer</footer>
 		</>
 	);
 }
